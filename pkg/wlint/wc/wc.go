6e2c779f602c3aab6a9e70047acf67f25245aced
Stephen Newell <snewell@users.noreply.github.com>
2023-10-29 19:19:22 -0600

    Teach word counter generic sorting and how to display relative occurances

--- pkg/wlint/wc/wc.go
+++ pkg/wlint/wc/wc.go
@@ -12,8 +12,24 @@ import (
 	"github.com/snewell/wlint-go/internal/wlint"
 )
 
+type countInfo struct {
+	word  string
+	count int
+}
+
 var (
 	caseSensitive bool
+	relativeUsage bool
+	sortMethod    string
+
+	sortMethods = map[string]func(countInfo, countInfo) bool{
+		"alpha": func(lhs countInfo, rhs countInfo) bool {
+			return lhs.word < rhs.word
+		},
+		"count": func(lhs countInfo, rhs countInfo) bool {
+			return lhs.count < rhs.count
+		},
+	}
 
 	wordCountCmd = &cobra.Command{
 		Use:   "word-count",
@@ -73,6 +89,10 @@ func countWordsCmd(args []string) error {
 		mapFn = strings.ToLower
 	}
 
+	sorter, found := sortMethods[sortMethod]
+	if !found {
+		return fmt.Errorf("unknown sort method: sortMehod")
+	}
 	counter := makeWordCounter(mapFn)
 	err = wlint.FilesOrStdin(args, func(r io.Reader) error {
 		purifier, err := makePurifier(r)
@@ -85,21 +105,39 @@ func countWordsCmd(args []string) error {
 		return err
 	}
 
-	keys := make([]string, len(counter.counts))
+	totalWords := 0
+	ci := make([]countInfo, len(counter.counts))
 	index := 0
-	for word := range counter.counts {
-		keys[index] = word
+	for word, count := range counter.counts {
+		ci[index].word = word
+		ci[index].count = count
+		totalWords += count
 		index++
 	}
-	sort.Strings(keys)
-	for _, word := range keys {
-		fmt.Printf("%v\t%v\n", word, counter.counts[word])
+
+	sort.Slice(ci, func(lhs int, rhs int) bool {
+		return sorter(ci[lhs], ci[rhs])
+	})
+	printer := func(index int) {
+		fmt.Printf("%v\t%v\n", ci[index].word, ci[index].count)
 	}
+	if relativeUsage {
+		totalWordsF := float64(totalWords)
+		printer = func(index int) {
+			fmt.Printf("%v\t%v\t%.3f%%\n", ci[index].word, ci[index].count, float64(ci[index].count)/totalWordsF*100)
+		}
+	}
+	for index := range ci {
+		printer(index)
+	}
+	fmt.Printf("Total words: %v\n", totalWords)
 	return err
 }
 
 func init() {
 	cmd.AddCommonFlags(wordCountCmd)
 	wordCountCmd.PersistentFlags().BoolVarP(&caseSensitive, "case-sensitive", "s", false, "Treat words as case sensitive")
+	wordCountCmd.PersistentFlags().BoolVarP(&relativeUsage, "relative", "r", false, "Include relative usage of each word")
+	wordCountCmd.PersistentFlags().StringVarP(&sortMethod, "sort-method", "m", "alpha", "Method to order output (options are alpha and count)")
 	cmd.AddCommand(wordCountCmd)
 }
